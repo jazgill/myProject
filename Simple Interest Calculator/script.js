003ae66ca23de4bdfe1e0fcd4aef23fc5d7ed439
Jaspal Gill <jsg@Jaspals-Air.lan>
2025-01-16 13:57:21 -0800

    Added full functionality of simple interest calculator with index.html, script.js and style.css updated.

--- Simple Interest Calculator/script.js
+++ Simple Interest Calculator/script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = principal * years * rate / 100;
+    let amount = parseInt(principal) + parseFloat(interest);
+    let result = document.getElementById("result");
+
+    let year = new Date().getFullYear() + parseInt(years);
+
+    if(principal <= 0){
+        alert("Please Enter a positive number.");
+        document.getElementById("principal").focus();
+    }
+    else {
+        result.innerHTML = `If you deposit <mark>$${principal}</mark>, <br>
+                    at an interest rate of <mark>${rate}%</mark>.<br>
+                    You will receive an amount of <mark>$${amount}</mark>,<br>
+                    in the year <mark>${year}</mark><br>`
+    }
+}
+function updateRate(){
+    let rateval = document.getElementById('rate').value;
+    document.getElementById('rate_val').innerText = rateval;
 }
         
